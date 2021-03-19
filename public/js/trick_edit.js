8feaea420011dd565db09a98eab440ebb415452b
kantum21 <quentinsporn@gmail.com>
2021-03-19 18:02:05 +0100

    Disable translation and optimize tricks links

--- public/js/trick_edit.js
+++ public/js/trick_edit.js
@@ -1,6 +1,5 @@
 //Edit main picture
 $("input[type=radio][name='trick_form[mainPicture]']").change(function () {
-
     var id_input = $("input[name='trick_form[mainPicture]']:checked:enabled").attr('id');
     var labelElement = $("label[for='" + id_input + "']");
     var imageElement = labelElement.children();
