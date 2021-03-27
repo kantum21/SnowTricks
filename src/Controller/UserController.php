39d110c9d17fa0c8a93eeb8d43cd8acf33c2d59b
kantum21 <quentinsporn@gmail.com>
2021-03-27 19:16:04 +0100

    CompleteUserSystem

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\ForgotPasswordType;
+use App\Form\ResetPasswordType;
 use App\Form\UserRegistrationType;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -14,7 +16,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -90,9 +91,10 @@ class UserController extends AbstractController
      */
     public function userActivate(EntityManagerInterface $em, UserRepository $repository, $username, $token)
     {
-        $user = $repository->findOneBy(['username' => $username]);
+        $user = $repository->findOneBy(['username' => $username, 'token' => $token]);
         if ($user && $token && $user->getToken() === $token)
         {
+            $user->setToken(null);
             $user->setActivated(true);
             $em->persist($user);
             $em->flush();
@@ -105,5 +107,91 @@ class UserController extends AbstractController
         }
 
         return new RedirectResponse($this->urlGenerator->generate('app_login'));
+    }
+
+    /**
+     * @Route("/forgot_password", name="forgot_password")
+     * @param Request $request
+     * @param UserRepository $repository
+     * @param EntityManagerInterface $em
+     * @return Response
+     * @throws Exception
+     */
+    public function forgotPassword(Request $request, UserRepository $repository, EntityManagerInterface $em)
+    {
+        $form = $this->createForm(ForgotPasswordType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $username = $form['username']->getData();
+            $user = $repository->findOneBy(['username' => $username]);
+            if (!$user)
+            {
+                $this->addFlash('danger', 'This user do not exist !');
+            }
+            else
+            {
+                $user->setToken(md5(random_bytes(10)));
+                $em->persist($user);
+                $em->flush();
+                self::sendPasswordResetEmail($user);
+                $this->addFlash('success', 'A password reset email has been sent for your account !');
+            }
+        }
+
+        return $this->render('user/forgot_password.html.twig', [
+            'form' => $form->createView()
+        ]);
+
+    }
+
+    protected function sendPasswordResetEmail(User $user)
+    {
+        $email = (new TemplatedEmail())
+            ->from('no-reply@snowtricks.com')
+            ->to($user->getEmail())
+            ->subject('Reset password for your SnowTricks account')
+            ->htmlTemplate('emails/reset_password.html.twig')
+            ->context([
+                'user' => $user
+            ])
+        ;
+
+        try {
+            $this->mailer->send($email);
+        } catch (TransportExceptionInterface $e) {
+            error_log($e->getMessage());
+        }
+    }
+
+    /**
+     * @Route("user/reset_password/{username}/{token}", name="reset_password")
+     * @param UserRepository $repository
+     * @param $username
+     * @param Request $request
+     * @param $token
+     * @param EntityManagerInterface $entityManager
+     * @return Response
+     */
+    public function resetPassword(UserRepository $repository, $username, Request $request, $token, EntityManagerInterface $entityManager)
+    {
+        $user = $repository->findOneBy(['username' => $username, 'token' => $token]);
+        if (!$user)
+            return new RedirectResponse($this->urlGenerator->generate('app_homepage'));
+        $form = $this->createForm(ResetPasswordType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $user->setToken(null);
+            $user->setPassword($this->encoder->encodePassword($user, $form['plainPassword']->getData()));
+            $entityManager->persist($user);
+            $entityManager->flush();
+            $this->addFlash('success', 'Password updated !');
+            return new RedirectResponse($this->urlGenerator->generate('app_homepage'));
+        }
+
+        return $this->render('user/reset_password.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
---
+++ src/Form/ForgotPasswordType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class ForgotPasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', null, [
+                'label' => 'Username *',
+                'constraints' => [
+                    new NotBlank()
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+        ]);
+    }
+}
